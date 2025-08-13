9f2fd351920a53cd259aeca89e8db67cda03b5a8
NarciSource <jwch11@gmail.com>
2025-08-13 11:56:50 +0900

    feat: 채팅 전송/구독 graphql 훅으로 변경

--- src/features/chat/ui/connect.vue
+++ src/features/chat/ui/connect.vue
@@ -15,18 +15,25 @@
 <script setup lang="ts">
 import { watchEffect } from "vue";
 
+import { Message, User } from "@/entities/chat/model";
+import { useReceiveMessageSubscription } from "../api/generated";
 import {
   connected,
   connect_failed,
   disconnected,
-  message_received,
   system_message_received,
-  handle_typing_message,
 } from "../service/event_helper";
 import useChatStore from "../store/useChatStore";
 
-const { insert_message, alarm_typing } = useChatStore();
+const { room, insert_message, alarm_typing } = useChatStore();
 const store = useChatStore();
+
+const { result: message_result } = useReceiveMessageSubscription({
+  roomId: room!.id!,
+});
+const { result: typing_result } = useReceiveMessageSubscription({
+  roomId: room!.id!,
+});
 
 watchEffect(() => {
   if (store.connecting) {
@@ -34,9 +41,15 @@ watchEffect(() => {
     connected(() => (store.connecting = true)); // 연결 성공
     connect_failed(() => (store.connecting = false)); // 연결 실패
     disconnected(() => (store.connecting = false)); // 연결 종료
-    message_received(insert_message); // 일반 메시지 수신
+
+    const { userId, content } = message_result.value?.message || {}; // 일반 메시지 수신
+    const received_messaged = new Message(new User(userId!), [content!]);
+    insert_message(received_messaged);
+
     system_message_received(insert_message); // 시스템 메시지 수신
-    handle_typing_message(alarm_typing); // 타이핑 알림
+
+    const { userId: typingId } = typing_result.value?.message || {}; // 타이핑 이벤트 수신
+    alarm_typing(typingId!);
   }
 });
 </script>
