e6a698394c85cb886531dc2f76e14f8cee78e348
NarciSource <jwch11@gmail.com>
2025-08-19 23:32:29 +0900

    refactor: socket.io 제거

--- src/features/chat/ui/connect.vue
+++ src/features/chat/ui/connect.vue
@@ -4,7 +4,7 @@
 
   <!-- 채팅 화면 접근 차단 -->
   <q-inner-loading
-    :showing="!store.connecting"
+    :showing="!connecting"
     class="fit"
     color="teal-9"
     label="채팅 연결 대기 중..."
@@ -13,7 +13,7 @@
 </template>
 
 <script setup lang="ts">
-import { watch, watchEffect } from "vue";
+import { watch } from "vue";
 import { storeToRefs } from "pinia";
 
 import { Message, User } from "@/entities/chat/model";
@@ -22,12 +22,10 @@ import {
   useReceiveMessageSubscription,
   useSystemSubscription,
 } from "../api/hooks";
-import { connected, connect_failed, disconnected } from "../service/event_helper";
 import useChatStore from "../store/useChatStore";
 
-const { room } = storeToRefs(useChatStore());
+const { room, connecting } = storeToRefs(useChatStore());
 const { insert_message, alarm_typing } = useChatStore();
-const store = useChatStore();
 
 const { result: message_result } = useReceiveMessageSubscription(() => ({
   roomId: room.value!.id!,
@@ -67,14 +65,5 @@ watch(typing_result, (result) => {
   if (!result) return;
 
   alarm_typing(result.typing.userId!);
-});
-
-watchEffect(() => {
-  if (store.connecting) {
-    // 소켓 이벤트 리스너 등록
-    connected(() => (store.connecting = true)); // 연결 성공
-    connect_failed(() => (store.connecting = false)); // 연결 실패
-    disconnected(() => (store.connecting = false)); // 연결 종료
-  }
 });
 </script>
