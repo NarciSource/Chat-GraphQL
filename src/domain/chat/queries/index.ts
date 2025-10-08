6d7ad2bed70d3ea173d1f64351ad0e2752284647
NarciSource <jwch11@gmail.com>
2025-10-08 19:16:37 +0900

    feat: SearchMessagesByUser 쿼리 추가

--- src/domain/chat/queries/index.ts
+++ src/domain/chat/queries/index.ts
@@ -1,5 +1,7 @@
 export { default as GetPartitionsQuery } from './GetPartitions.query';
 export { default as GetMessageHistoryQuery } from './GetMessageHistory.query';
+export { default as SearchMessagesByUserQuery } from './SearchMessagesByUser.query';
 
 export { default as GetPartitionsHandler } from './GetPartitions.handler';
 export { default as GetMessageHistoryHandler } from './GetMessageHistory.handler';
+export { default as SearchMessagesByUserHandler } from './SearchMessagesByUser.handler';
