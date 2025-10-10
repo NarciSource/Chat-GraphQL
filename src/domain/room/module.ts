877801966e1f8175f8afa7bad186ae3f10a1451e
NarciSource <jwch11@gmail.com>
2025-10-11 00:17:15 +0900

    feat: GetRoomsByUser 쿼리 추가

--- src/domain/room/module.ts
+++ src/domain/room/module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 
 import { RepositoryModule } from 'src/repository';
+import * as queries from './quires';
 import * as commands from './commands';
 import { RoomMutationResolver, RoomSubscriptionResolver } from './resolvers';
 
 @Module({
   imports: [CqrsModule, RepositoryModule],
-  providers: [RoomMutationResolver, RoomSubscriptionResolver, ...Object.values(commands)],
+  providers: [
+    RoomMutationResolver,
+    RoomSubscriptionResolver,
+    ...Object.values(queries),
+    ...Object.values(commands),
+  ],
 })
 export default class RoomModule {}
